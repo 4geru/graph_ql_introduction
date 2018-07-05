f24fa7d422f761b54fdf4b00d4441b920b73e733
Rp7rf <westhouse51@gmail.com>
2018-07-05 23:28:47 +0900

    add pagenation

--- app/graphql/types/query_type.rb
+++ app/graphql/types/query_type.rb
@@ -2,6 +2,7 @@ Types::QueryType = GraphQL::ObjectType.define do
   name 'Query'
 
   field :allLinks, function: Resolvers::LinksSearch
+  
   # queries are just represented as fields
   # field :allLinks, !types[Types::LinkType] do
   #   # resolve would be called in order to fetch data for that field
